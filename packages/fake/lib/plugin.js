199fe762102ca8b19f803b6cd6fb82252de6e6b5
Ayan Khan <khanayan_123@hotmail.com>
2021-05-18 18:20:42 -0400

    chore: add support for plugin cli args

--- packages/fake/lib/plugin.js
+++ packages/fake/lib/plugin.js
@@ -5,11 +5,22 @@ import B from 'bluebird';
 
 
 export default class FakePlugin extends BasePlugin {
+  constructor (pluginName, pluginArgs = {}) {
+    super(pluginName, pluginArgs);
+  }
+
+  async getFakeCliArgs () {
+    await B.delay(1);
+    return this.pluginArgs;
+  }
 
   static newMethodMap = {
     '/session/:sessionId/fake_data': {
       GET: {command: 'getFakeSessionData'},
       POST: {command: 'setFakeSessionData', payloadParams: {required: ['data']}}
+    },
+    '/session/:sessionId/fakecliargs': {
+      GET: {command: 'getFakeCliArgs'}
     },
   };
 
