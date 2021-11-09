296259edb38301749df48b3b3ed7a05289204b0d
Christopher Hiller <boneskull@boneskull.com>
2021-11-09 13:49:31 -0800

    fix(build): avoid build failures and rejigger types
    I could not build this project locally, so I set out to fix it.  I think I did?  There were problems especially with wdio, and I know from experience that `ts-node` is required to address them.  I have also added `@types/mocha`, but we're not doing too much building
    
    Anyway, we now have incremental builds!  That means "it's faster" since TS will only build one project at a time instead of all of them.
    
    I was able to remove `build.js`, since `tsc -b` does the same thing now.  I was unable to retain the `index.d.ts` in the `base` plugin (though could probably figure it out if I spent more time with it); instead, the definitions are now in the docstrings of `plugin.js`.  All declarations are now generated by `tsc` and are output alongside the output `.js` files.  Please note that I did have to actually implement a couple functions which were previously commented-out.  We will want to define a `Plugin` interface later which allows the `<cmd>` methods. Since these don't exist in `BasePlugin`, we should not attempt to define them in this type.
    
    Not too much actual _type-checking_ is happening here, but we can enable that later (per file via `// @ts-check` pragma, or a `checkJs: true` in a project's `tsconfig.json`).

--- packages/base/lib/plugin.js
+++ packages/base/lib/plugin.js
@@ -1,17 +1,21 @@
-import { logger } from '@appium/support';
+import {logger} from '@appium/support';
 
-
+/**
+ * @abstract
+ */
 export default class BasePlugin {
-
-  // plugins can define new methods for the Appium server to map to command names, of the same
-  // format as used in Appium's routes.js, for example, this would be a valid newMethodMap:
-  //
-  // {
-  //   '/session/:sessionId/new_method': {
-  //      GET: {command: 'getNewThing'},
-  //      POST: {command: 'setNewThing', payloadParams: {required: ['someParam']}}
-  //   }
-  // }
+  /**
+   * Plugins can define new methods for the Appium server to map to command names, of the same
+   * format as used in Appium's routes.js, for example, this would be a valid newMethodMap:
+   * @type {Record<string,import('express').IRoute}
+   * @example
+   * {
+   *   '/session/:sessionId/new_method': {
+   *     GET: {command: 'getNewThing'},
+   *     POST: {command: 'setNewThing', payloadParams: {required: ['someParam']}}
+   *   }
+   * }
+   */
   static newMethodMap = {};
 
   constructor (pluginName, opts = {}) {
@@ -22,7 +26,6 @@ export default class BasePlugin {
     this.opts = opts;
   }
 
-
   /**
    * Override to return a validator object used to validate CLI args passed into appium as
    * --plugin-args='{"yourPluginName": {"argName": argVal, ...}}'
@@ -30,7 +33,6 @@ export default class BasePlugin {
   static get argsConstraints () {
     return {};
   }
-
 
   /**
    * Optionally updates an Appium express app and http server, by calling methods that may mutate
@@ -41,11 +43,13 @@ export default class BasePlugin {
    * In order to add a new route to Appium with this plugin. Or, you could add new listeners to the
    * httpServer object.
    *
-   * @param {object} expressApp - the Express 'app' object used by Appium for route handling
-   * @param {http.Server} httpServer - the node HTTP server that hosts the app
+   * @param {import('express').Application} expressApp - the Express 'app' object used by Appium for route handling
+   * @param {import('http').Server} httpServer - the node HTTP server that hosts the app
    */
-  /*static async updateServer (expressApp, httpServer) {
-  }*/
+  // eslint-disable-next-line no-unused-vars, require-await
+  static async updateServer (expressApp, httpServer) {
+    // throw new TypeError('Not yet implemented!');
+  }
 
   /**
    * Handle an Appium command, optionally running and using or throwing away the value of the
@@ -73,9 +77,33 @@ export default class BasePlugin {
 
   /**
    * You could also handle all commands generically by implementing 'handle'
+   *
+   * @param {() => Promise<void>} next - A reference to an async function which
+   * encapsulates what would normally happen if this plugin were not handling a
+   * command.
+   * @param {BaseDriver} driver - the instance of the Appium driver currently
+   * handling commands
+   * @param {string} cmdName - the name of the command being handled
+   * @param {...object} args - the args that would be applied to the command
+   * @returns {Promise<any>}
    */
-  /*async handle (next, driver, cmdName, ...args) {
-     return await next();
-  }*/
+  // eslint-disable-next-line no-unused-vars
+  async handle (next, driver, cmdName, ...args) {
+    return await next();
+  }
 
+  /**
+   * @param {method} string
+   * @param {route} string
+   * @param {any} body
+   * @returns {boolean}
+   */
+  // eslint-disable-next-line no-unused-vars
+  shouldAvoidProxy (method, route, body) {
+    throw new TypeError('Not yet implemented!');
+  }
 }
+
+/**
+ * @typedef {import('@appium/base-driver').BaseDriver} BaseDriver
+ */
